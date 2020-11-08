bd4d595ba6edb55aafa063a4dd9ceeaf9b24e534
SirBuster33 <sirbuster333@gmail.com>
2020-11-08 14:21:30 +0100

    Inserted queries to communicate with database.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,11 +1,14 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 
 public class Controller {
 
@@ -15,18 +18,39 @@ public class Controller {
     public TableColumn tableColumnStudentName;
     public TableColumn tableColumnStudentCity;
 
+    ObservableList<Student> students = FXCollections.observableArrayList();
+
     public void initialize(){DatabaseManipulator DB = new DatabaseManipulator();
 
+        tableStudents.setItems(students);
         try{
             String url = "jdbc:sqlite:C:\\Users\\Kata\\Documents\\RUC stuff from small lenovo\\Ruc\\Philipp RUC" +
                     "\\5th Semester\\Portfolio3_SoftwareDevelopment\\src\\Student_Database\\Student_Database";
             DB.connect(url);
             DB.createStatement();
 
-            ResultSet rs = DB.studentQueryStatement();
+            ResultSet rs;
+            System.out.println("Running method studentQueryStatement...");
+            rs = DB.studentQueryStatement();
+            System.out.println("\nRunning method studentInputStatement...");
+            rs = DB.studentInputStatement();
+            System.out.println("\nRunning method sqlPlanPreparedStatement... (Not working as intended)");
+            rs = DB.sqlPlanPreparedStatement("C1", "C3", 7);
         }
         catch (SQLException e){
             e.printStackTrace();
+            System.out.println("Error in try-catch line 37");
+        }
+        finally {
+            if (DB.conn != null){
+                try{
+                    System.out.println("Closing connection...");
+                    DB.closeConnection();
+                } catch (SQLException e){
+                    e.printStackTrace();
+                    System.out.println("Error in 'finally'!");
+                }
+            }
         }
 
 
--- src/sample/DatabaseManipulator.java
+++ src/sample/DatabaseManipulator.java
@@ -1,6 +1,7 @@
 package sample;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import javax.xml.transform.Result;
 import java.sql.*;
 import java.util.Scanner;
 import static java.sql.DriverManager.getConnection;
@@ -17,7 +18,12 @@ public class DatabaseManipulator {
         conn = getConnection(url);
     }
 
+    public void closeConnection() throws SQLException {
+        conn.close();
+    }
+
     public ResultSet studentQueryStatement() throws SQLException {
+        System.out.println("Running method studentQueryStatement...");
 
         String sql = "SELECT * FROM student;";
         ResultSet rs = stmt.executeQuery(sql);
@@ -30,6 +36,53 @@ public class DatabaseManipulator {
             String Name = rs.getString(2);
             String City = rs.getString(3);
             System.out.println(ID + " " + Name + " " + City);
+        }
+        return rs;
+    }
+
+    public ResultSet studentInputStatement() throws SQLException {
+        System.out.println("\nRunning method studentInputStatement...");
+
+        System.out.println("Type the student ID you want info for: ");
+        Scanner scanner = new Scanner(System.in);
+        String studentID = scanner.nextLine();
+
+        String sql = "SELECT * FROM student WHERE ID = '" + studentID + "';";
+        ResultSet rs = stmt.executeQuery(sql);
+
+        if (rs == null){
+            System.out.println("No records retrieved.");
+        }
+        while (rs != null && rs.next()){
+            String ID = rs.getString(1);
+            String Name = rs.getString(2);
+            String City = rs.getString(3);
+            System.out.println("Info for Student with ID " + ID + ": " + Name + ": " + City + ".");
+        }
+        return rs;
+    }
+
+    public ResultSet sqlPlanPreparedStatement(String courseIDS, String courseSD, Integer grade) throws SQLException {
+        System.out.println("Running preparedStatement...");
+        String sql = "SELECT G1.StudentID, G1.CourseID, G1.Grade, G2.CourseID, G2.Grade " +
+                "FROM grade as G1 " +
+                "JOIN grade as G2 on G1.StudentID = G2.StudentID " +
+                "WHERE G1.CourseID = ? AND G2.CourseID = ? AND G1.Grade > ? " +
+                "AND G1.Grade = G2.Grade;";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, courseIDS);
+        pstmt.setString(2, courseSD);
+        pstmt.setInt(3, grade);
+        ResultSet rs = pstmt.executeQuery();
+        if (rs == null){
+            System.out.println("No records retrieved");
+        }
+        while (rs != null && rs.next()){
+            System.out.println("Printing out rs.getStuff...");
+            System.out.println(rs.getString(1) + " " + rs.getString(2) + " "
+                    + rs.getInt(3) + " " + rs.getString(4) + " "
+                    + rs.getInt(5));
+            System.out.println("Done printing out rs.getStuff.");
         }
         return rs;
     }
